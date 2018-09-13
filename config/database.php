f106572a18049a01e9e8de28015e974388bfb77f
Nickolay Kolesnikov <kolyan654d@gmail.com>
2018-09-14 02:45:58 +0300

    procfile

--- config/database.php
+++ config/database.php
@@ -80,6 +80,19 @@ return [
             'schema' => 'public',
             'sslmode' => 'require',
         ],
+        
+        'pgsql' => [
+      'driver' => 'pgsql',
+        'host' => env('DB_HOST', 'ec2-12-34-56-789.compute-1.amazonaws.com'),
+        'port' => env('DB_PORT', '5432'),
+        'database' => env('DB_DATABASE', 'dbXXXXXXXXXXXX'),
+        'username' => env('DB_USERNAME', 'XXXXXXXXXXXXXX'),
+        'password' => env('DB_PASSWORD', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'),
+        'charset' => 'utf8',
+        'prefix' => '',
+        'schema' => 'public',
+        'sslmode' => 'prefer',
+    ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
