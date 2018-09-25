ab142cf399e5f0ac5436b08fd1acec76d69fec3c
Nickolay Kolesnikov <kolyan654d@gmail.com>
2018-09-25 21:26:49 +0300

    stylezed

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
   <div class="container text-center">
-    <form class="form" action="{{ route('projects.update', ['id'=> $project->id]) }}" method="post">
-        <input type="text" name="name" value="{{ $project->name }}">
-        <input type="hidden" name="_method" value="PUT">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="submit" class="btn btn-default" value="update">
+    <form action="{{ route('projects.update', ['id'=> $project->id]) }}" method="post" class="form">
+      <input type="text" name="name" value="{{ $project->name }}" class="form-control input-edit">
+      <input type="hidden" name="_method" value="PUT">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      <input type="submit" class="btn btn-default" value="update">
     </form>
   </div>
 @endsection
